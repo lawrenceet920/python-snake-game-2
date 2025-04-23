3447621f863a4f25a314066467e59734b7ab41d9
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2025-04-23 13:52:29 -0400

    Game functions

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ CELL_SIZE = 10
 # Snake globals
 direction = 1 # ^v<>
 score = 0
+clicked = False
 # Generic Functions
 def init_game():
     pygame.init()
@@ -42,35 +43,28 @@ def init_game():
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption(TITLE)
     return screen
-def handle_events(game_over):
-    global direction
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+def handle_events():
+        global direction
+        global clicked
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return False
-            if event.key == pygame.K_w and direction != 2:
-                direction = 1
-            if event.key == pygame.K_s and direction != 1:
-                direction = 2
-            if event.key == pygame.K_a and direction != 4:
-                direction = 3
-            if event.key == pygame.K_d and direction != 3:
-                direction = 4
-        if game_over:
-            img = pygame.font.SysFont(None, 40).render('Game Over!', True, RED)
-            screen.blit(img, ((WINDOW_WIDTH//2)-(img.get_width()/2), (WINDOW_HEIGHT//2)-(img.get_height()/2)))
-
-            
-            restart_img = pygame.font.SysFont(None, 30).render('Play again?', True, CYAN)
-            screen.blit(restart_img, ((WINDOW_WIDTH//2)-(restart_img.get_width()/2), (WINDOW_HEIGHT//2)-(restart_img.get_height()/2)+(img.get_height()*3)))
-            
-            pygame.draw.rect(screen, RED, ((WINDOW_WIDTH//2)-(restart_img.get_width()/2), (WINDOW_HEIGHT//2)-(restart_img.get_height()/2)+(img.get_height()*3)))
-            # Click play again?
-            # if event.type == pygame.MOUSEBUTTONDOWN:
-                # if again_rect.collidepoint(pygame.mouse.get_pos()):
-    return True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+                if event.key == pygame.K_w and direction != 2:
+                    direction = 1
+                if event.key == pygame.K_s and direction != 1:
+                    direction = 2
+                if event.key == pygame.K_a and direction != 4:
+                    direction = 3
+                if event.key == pygame.K_d and direction != 3:
+                    direction = 4
+            if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+                clicked = True
+            if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+                clicked = False
+        return True
 def main():
     clock = pygame.time.Clock()
     running = True
@@ -83,44 +77,41 @@ def main():
     new_piece = [0, 0]
     new_food = True
     key_frame = 0
+    game_over = False
 
     while running:
         global score
+        global clicked
+        global direction
         # While Running
         draw_screen()
         draw_score()
-        head = True
-        for x in snake:
-            if head == False:
-                pygame.draw.rect(screen, (0, 100, 0), (x[0], x[1], CELL_SIZE, CELL_SIZE))
-                pygame.draw.rect(screen, GREEN, (x[0]+1, x[1]+1, CELL_SIZE -2, CELL_SIZE-2))
-            if head == True:
-                pygame.draw.rect(screen, (100, 0, 0), (x[0], x[1], CELL_SIZE, CELL_SIZE))
-                pygame.draw.rect(screen, RED, (x[0]+1, x[1]+1, CELL_SIZE -2, CELL_SIZE-2))
-                head = False
-        running = handle_events(game_over)
-        # Food
-        if new_food:
-            new_food = False
-            food[0] = CELL_SIZE* random.randint(0, (WINDOW_WIDTH // CELL_SIZE)-1)
-            food[1] = CELL_SIZE* random.randint(0, (WINDOW_HEIGHT // CELL_SIZE)-1)
-        pygame.draw.rect(screen, RED, (food[0], food[1], CELL_SIZE, CELL_SIZE))
-        
-        if food == snake[0]:
-            new_food = True
-            score += 1
-            new_piece = list(snake[-1])
-            if direction == 1:
-                new_piece[1] += CELL_SIZE
-            if direction == 2:
-                new_piece[1] -= CELL_SIZE
-            if direction == 3:
-                new_piece[0] -= CELL_SIZE
-            if direction == 4:
-                new_piece[0] += CELL_SIZE
-            snake.append(new_piece)
-        game_over = check_game_over(snake):
-        if not game_over
+        running = handle_events()
+        if game_over:
+            gameover_img = pygame.font.SysFont(None, 40).render('Game Over!', True, RED)
+            restart_img = pygame.font.SysFont(None, 30).render('Play again?', True, CYAN)
+            restart_rect = pygame.Rect((WINDOW_WIDTH//2)-(restart_img.get_width()/2), (WINDOW_HEIGHT//2)-(restart_img.get_height()/2)+(gameover_img.get_height()*3), (restart_img.get_width()), (restart_img.get_height()))
+
+            pygame.draw.rect(screen, RED, restart_rect)
+            screen.blit(gameover_img, ((WINDOW_WIDTH//2)-(gameover_img.get_width()/2), (WINDOW_HEIGHT//2)-(gameover_img.get_height()/2)))
+            screen.blit(restart_img, ((WINDOW_WIDTH//2)-(restart_img.get_width()/2), (WINDOW_HEIGHT//2)-(restart_img.get_height()/2)+(gameover_img.get_height()*3)))
+            
+            if clicked:
+                if restart_rect.collidepoint(pygame.mouse.get_pos()):
+                    clicked = 'Resetting!'
+                    snake = [[int(WINDOW_WIDTH/2), int(WINDOW_HEIGHT/2)]]
+                    snake.append([int(WINDOW_WIDTH/2), int(WINDOW_HEIGHT/2) + CELL_SIZE*1])
+                    snake.append([int(WINDOW_WIDTH/2), int(WINDOW_HEIGHT/2) + CELL_SIZE*2])
+                    snake.append([int(WINDOW_WIDTH/2), int(WINDOW_HEIGHT/2) + CELL_SIZE*3])
+                    food = [0, 0]
+                    new_piece = [0, 0]
+                    new_food = True
+                    key_frame = 0
+                    game_over = False
+                    print(clicked)
+                    clicked = False
+                    direction = 1
+        else:
             # Movement
             key_frame += 1
             if key_frame >= FPS/10:
@@ -137,6 +128,37 @@ def main():
                     snake[0][0] -= CELL_SIZE
                 if direction == 4:
                     snake[0][0] += CELL_SIZE
+            # Food
+            if new_food:
+                new_food = False
+                food[0] = CELL_SIZE* random.randint(0, (WINDOW_WIDTH // CELL_SIZE)-1)
+                food[1] = CELL_SIZE* random.randint(0, (WINDOW_HEIGHT // CELL_SIZE)-1)
+            
+            if food == snake[0]:
+                new_food = True
+                score += 1
+                new_piece = list(snake[-1])
+                if direction == 1:
+                    new_piece[1] += CELL_SIZE
+                if direction == 2:
+                    new_piece[1] -= CELL_SIZE
+                if direction == 3:
+                    new_piece[0] -= CELL_SIZE
+                if direction == 4:
+                    new_piece[0] += CELL_SIZE
+                snake.append(new_piece)
+            game_over = check_game_over(snake)
+        # End of gave over if statement
+        head = True
+        for x in snake:
+            if head == False:
+                pygame.draw.rect(screen, (0, 100, 0), (x[0], x[1], CELL_SIZE, CELL_SIZE))
+                pygame.draw.rect(screen, GREEN, (x[0]+1, x[1]+1, CELL_SIZE -2, CELL_SIZE-2))
+            if head == True:
+                pygame.draw.rect(screen, (100, 0, 0), (x[0], x[1], CELL_SIZE, CELL_SIZE))
+                pygame.draw.rect(screen, RED, (x[0]+1, x[1]+1, CELL_SIZE -2, CELL_SIZE-2))
+                head = False
+        pygame.draw.rect(screen, RED, (food[0], food[1], CELL_SIZE, CELL_SIZE))
         # Limit clock to FPS & Update Screen
         pygame.display.flip()
         clock.tick(FPS)
